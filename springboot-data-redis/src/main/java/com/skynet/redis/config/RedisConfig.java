a6ce78c60b1dca4043ec090d94cd7c4c7a9a5781
章文平 <1079895646@qq.com>
2021-01-25 17:33:04 +0800

    springboot简单案例学习

---
+++ springboot-data-redis/src/main/java/com/skynet/redis/config/RedisConfig.java
@@ -0,0 +1,112 @@
+package com.skynet.redis.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String hostName;
+
+    @Value("${spring.redis.port}")
+    private Integer port;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.database}")
+    private Integer database;
+
+    @Value("${spring.redis.jedis.pool.max-idle}")
+    private Integer maxIdle;
+
+    @Value("${spring.redis.jedis.pool.min-idle}")
+    private Integer mimIdle;
+
+    @Value("${spring.redis.jedis.pool.max-active}")
+    private Integer maxActive;
+
+    @Value("${spring.redis.jedis.pool.max-wait}")
+    private Duration maxWaitMillis;
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConfigurationProperties(prefix = "spring.redis.jedis.pool")
+    public JedisPoolConfig jedisPoolConfig(){
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        jedisPoolConfig.setMaxIdle(maxIdle);
+        jedisPoolConfig.setMinIdle(mimIdle);
+        jedisPoolConfig.setMaxTotal(maxActive);
+        jedisPoolConfig.setMaxWaitMillis(maxWaitMillis.toMillis());
+        jedisPoolConfig.setTestOnBorrow(true);
+        jedisPoolConfig.setTestOnReturn(true);
+        return jedisPoolConfig;
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConfigurationProperties(prefix = "spring.redis")
+    public RedisConnectionFactory redisConnectionFactory (){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setDatabase(database);
+        redisStandaloneConfiguration.setPassword(password);
+        redisStandaloneConfiguration.setPort(port);
+        JedisClientConfiguration.JedisPoolingClientConfigurationBuilder jpcb = (JedisClientConfiguration.JedisPoolingClientConfigurationBuilder) JedisClientConfiguration.builder();
+        jpcb.poolConfig(this.jedisPoolConfig());
+        JedisClientConfiguration jedisClientConfiguration = jpcb.build();
+        return new JedisConnectionFactory(redisStandaloneConfiguration, jedisClientConfiguration);
+    }
+
+
+    @Primary
+    @Bean(name = "myRedisTemplate")
+    @ConditionalOnMissingBean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值（默认使用JDK的序列化方式）
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        //使用StringRedisSerializer来序列化和反序列化redis的key值
+        RedisSerializer redisSerializer = new StringRedisSerializer();
+        //key
+        redisTemplate.setKeySerializer(redisSerializer);
+        redisTemplate.setHashKeySerializer(redisSerializer);
+        //value
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+
+}
---
+++ springboot-data-redis/src/main/java/com/skynet/redis/controller/RedisController.java
@@ -0,0 +1,85 @@
+package com.skynet.redis.controller;
+
+import com.alibaba.fastjson.JSONArray;
+import com.skynet.redis.entity.User;
+import io.micrometer.core.lang.Nullable;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class RedisController {
+
+    @Resource(name = "myRedisTemplate")
+    private RedisTemplate redisTemplate;
+
+    @GetMapping(path = "/string/set")
+    public ResponseEntity<String> set(@RequestParam("redisKey") String redisKey) {
+        log.info("redisKey:" + redisKey);
+        User user = new User();
+        user.setUserId(1);
+        user.setAge(18);
+        user.setUserName("章文平");
+        user.setAddress("上海市浦东新区唐镇创新中路");
+        Object object = JSONArray.toJSON(user);
+        String jsonString = object.toString();
+        redisTemplate.opsForValue().set(redisKey, jsonString);
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping(path = "/string/get")
+    public ResponseEntity<User> get(@RequestParam("redisKey") String redisKey) {
+        log.info("redisKey:" + redisKey);
+        String jsonString = (String) redisTemplate.opsForValue().get(redisKey);
+        User user = JSONArray.parseObject(jsonString, User.class);
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(path = "/pipeline/execute")
+    public ResponseEntity<List<User>> execute(@RequestParam("redisKey") String redisKey) {
+
+        List<String> redisKeyList = new ArrayList<String>();
+        List<User> userList = new ArrayList<User>();
+
+        redisKeyList.add("CONFIG:API");
+        redisKeyList.add("CONFIG:APP");
+
+        List<String> stringList = redisTemplate.executePipelined(new RedisCallback<String>() {
+            @Nullable
+            @Override
+            public String doInRedis(RedisConnection connection) throws DataAccessException {
+                connection.openPipeline();
+
+                if(CollectionUtils.isEmpty(redisKeyList)){
+                    for (String redisKey : redisKeyList) {
+                        System.out.println("redisKey:" + redisKey);
+                        connection.get(redisKey.getBytes());
+                    }
+                }
+
+                List<Object> results = connection.closePipeline();
+                System.out.println("results:" + results);
+                return null;
+            }
+        });
+
+        for (String userString : stringList) {
+            User user = JSONArray.parseObject(userString, User.class);
+            userList.add(user);
+        }
+
+        return ResponseEntity.ok(userList);
+    }
+}
